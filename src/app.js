71445e0482e2b09d9557ce186610c735ae86db5f
shivam kumar <sk366035@gmail.com>
2026-08-19 10:31:56 +0530

    utils code added

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
+
+
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.CORS_ORIGIN,
+        Credentials: true
+    }
+))
+
+app.use(express.json({ limit: "16kb" }))
+
+
+
+export { app }
+
