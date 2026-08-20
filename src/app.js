78b4a78819b6a96bfd140e7c9f45560c5129d34f
shivam kumar <sk366035@gmail.com>
2026-08-20 09:46:23 +0530

    Server and database connection

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(cors(
 ))
 
 app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 
 
--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ connectDB()
     })
 
 
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+
 
 
 
